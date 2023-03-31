0d7dd28db3cfb03336218fb207941c4d49180d9f
udit-1211 <uditparekh1@gmail.com>
2023-03-31 23:01:18 +0530

    Initial commit

---
+++ Placement-Portal-main/php/footer.php
@@ -0,0 +1,44 @@
+<footer id="footer" class="footer">
+
+
+
+    <div class="footer-legal text-center">
+        <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+            <div class="d-flex flex-column align-items-center align-items-lg-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>CDPC</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+
+                    <a href="https://www.charusat.ac.in/cdpc.php/">-CDPC</a>
+                </div>
+            </div>
+
+            <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+                <a href="https://twitter.com/thecharusat?lang=en" class="twitter" target="_blank"><i class="bi bi-twitter"></i></a>
+                <a href="https://www.facebook.com/thecharusat/" class="facebook" target="_blank"><i class="bi bi-facebook"></i></a>
+                <!-- <a href="#" class="instagram"><i class="bi bi-instagram"></i></a> -->
+                <!-- <a href="#" class="google-plus"><i class="bi bi-skype"></i></a> -->
+                <a href="https://www.linkedin.com/school/charotar-university-of-science-and-technology/" class="linkedin" target="_blank"><i class="bi bi-linkedin"></i></a>
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
